1d75590f85c39453bc6f5b71991b8f4af63bb9de
Bruna <brunaalmeida075@gmail.com>
2022-08-19 10:55:04 -0300

    exercicios javascript

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/aula2.js
@@ -0,0 +1,68 @@
+const a = 0;
+const b = 7;
+const c = 15;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// if( a > b){
+//     console.log(a)
+// }else{console.log ('não')}
+
+
+// if(a > b && a> c){
+//    console.log(a)
+// } else if ( b > a && b > c){
+//     console.log(b)
+// }else if ( c > a && c > b){
+//     console.log(c)
+// }
+// let valor ='positivo'
+// valor = 0;
+// valor= 'negativo';
+// switch (valor) {
+//     case 'positivo':
+//         console.log('positivo')
+//         break;
+//     case 'negativo':
+//         console.log('negativo')
+//         break;
+//     default :
+//     console.log('Zero')
+//         break;
+// }
+// let angulo1 = 60; angulo2 = 130; angulo3 = 180;
+// let triangulo = 180;
+// if (triangulo != angulo1 && triangulo != angulo2 && triangulo != angulo3){
+//     console.log(false)
+//  }else if (triangulo == angulo1 || angulo2 || angulo3){
+//     console.log(true)
+//  }else{
+//     console.log("erro programa invalido")
+//  }
+const d = 2;
+const e = 3;
+const f =9;
+const par= 0;
+const impar = 1;
+// switch(d%2 && e%2 && f%2){
+//     case par:
+//         console.log(true)
+//         break;
+//     case impar :
+//         console.log(false)
+//         break;
+
+// }
+// if(par == d%2 || e%2 || f%2){
+//     console.log(true)
+// }else if(impar == d%2 || e%2 || f%2){
+//     console.log(false)
+// }
+if(impar == d%2 || e%2 || f%2){
+    console.log(true)
+}else if(par == d%2 || e%2 || f%2){
+    console.log(false)
+}
