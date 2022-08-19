280051807958e5ca0e242df23d9db2fe55c79e4a
Bruna <brunaalmeida075@gmail.com>
2022-08-19 18:20:24 -0300

    exercicios de fixação

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/java.js
@@ -0,0 +1,29 @@
+// ///////exercicio 1//////////////////
+
+// let menu = ['home', 'serviços', 'portifólio', 'links'];
+// let menuserviço = menu[1]
+
+// console.log(menuserviço)
+
+// ///////////exercicio 2///////////////
+// console.log(menu.indexOf('portifólio'))
+
+// ///////////exercicio 3//////////////
+
+// menu.push('contato')
+
+// console.log(menu);
+
+/////////////exercicio 4////////////////
+
+// let groceryList = ['arroz', 'feijao', 'alface', 'melancia'];
+// for ( let i = 0; i < groceryList.length; i += 1){
+//     console.log(groceryList[i])
+// }
+
+//////////////exercicio 5///////////////
+
+let name = ['joão', 'maria', 'antonio', 'margarida'];
+for (let res of name) {
+    console.log(res)
+}
