a72d96c7a7c52177283511553830f63a490bc409
Bruna <brunaalmeida075@gmail.com>
2022-09-17 09:09:24 -0300

    exercicios

--- secao-7-introducao-a-JavaScript-ES6-e-testes-unitarios/dia-1-javaScript-ES6-let-const-arrow-function-ternaty-operador/script.js
+++ secao-7-introducao-a-JavaScript-ES6-e-testes-unitarios/dia-1-javaScript-ES6-let-const-arrow-function-ternaty-operador/script.js
@@ -1,15 +1,139 @@
-function testingScope(escopo) {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = (`${ifScope} ótimo, fui utilizada no escopo !`) ;
-      console.log(ifScope)
-    } else {
-      const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-      console.log(elseScope);
-    }
-  }
-  testingScope(false);
-  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-  oddsAndEvens.sort((a,b) => a-b);
-  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); 
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = (`${ifScope} ótimo, fui utilizada no escopo !`) ;
+//       console.log(ifScope)
+//     } else {
+//       const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+//   testingScope(false);
+//   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+//   oddsAndEvens.sort((a,b) => a-b);
+//   console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); 
+// const sum = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+//   return value1 + value2;
+// };
 
+// console.log(sum(2, '3'));
+// const verifyIsNumber = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+// };
+
+// const sum = (value1, value2) => {
+//   try {
+//     verifyIsNumber(value1, value2);
+//     return value1 + value2;
+//   } catch (error) {
+//     return error.message;
+//   }
+// };
+
+// console.log(sum(2, '3'));
+// const coolestTvShow = {
+//   name: 'BoJack Horseman',
+//   genre: 'adult animation',
+//   createdBy: 'Raphael Bob-Waksberg',
+//   favoriteCharacter: 'Princess Carolyn',
+//   quote: 'Princess Carolyn always lands on her feet.',
+//   seasons: 6,
+// };
+
+// // for (const property in coolestTvShow) {
+// //   console.log(property);
+// // }
+
+// console.log(Object.keys(coolestTvShow));
+
+// // [ 'name', 'genre', 'createdBy', 'favoriteCharacter', 'quote', 'seasons' ]
+
+// const student1 = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkills: 'Ótimo',
+// };
+
+// const student2 = {
+//   Html: 'Bom',
+//   Css: 'Ótimo',
+//   JavaScript: 'Ruim',
+//   SoftSkills: 'Ótimo',
+//   Git: 'Bom', // chave adicionada
+// };
+
+// const listSkills = (student) => {
+//   const arrayOfSkills = Object.keys(student);
+//   for (const index in arrayOfSkills) {
+//     console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//   }
+// };
+
+// console.log('Estudante 1');
+// listSkills(student1);
+
+// console.log('Estudante 2');
+// listSkills(student2);
+
+// const student = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkill: 'Ótimo',
+// };
+
+// const listSkillsValuesWithFor = (student) => {
+//   const skills = [];
+//   for(skill in student) {
+//     skills.push(student[skill]);
+//   }
+
+//   return skills;
+// };
+
+// const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// // Sem Object.values
+// console.log(listSkillsValuesWithFor(student));
+
+// // Com Object.values
+// console.log(listSkillsValuesWithValues(student));
+
+// const person = {
+//   name: 'Alberto',
+//   lastName: 'Gomes',
+//   age: 20,
+// };
+
+// const info = {
+//   age: 23,
+//   job: 'engenheiro',
+// };
+
+// const family = {
+//   children: ['Maria', 'João'],
+//   wife: 'Ana',
+// };
+
+// Object.assign(person, info, family)
+// console.log(person)
+
+
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
