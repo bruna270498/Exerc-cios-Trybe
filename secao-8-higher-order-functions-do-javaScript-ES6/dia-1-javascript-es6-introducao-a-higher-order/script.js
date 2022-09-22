305ec74447d7aac1dc78840161c1464a56694367
Bruna <brunaalmeida075@gmail.com>
2022-09-22 15:55:24 -0300

    iniciando seçao

---
+++ secao-8-higher-order-functions-do-javaScript-ES6/dia-1-javascript-es6-introducao-a-higher-order/script.js
@@ -0,0 +1,9 @@
+const manha = () => 'Acordando!!' ;
+const cafe = () => 'Bora tomar café!!' ;
+const noite = () => 'Partiu dormir!!' ;
+
+const doingThings = (manha) => manha ;
+
+console.log(doingThings(manha))
+console.log(doingThings(cafe))
+console.log(doingThings(noite))
